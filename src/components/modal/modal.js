d9d2c28401fe349d65dab606b87228f8631dc942
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2023-05-26 09:00:41 -0300

    fix: modal btn

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -17,7 +17,6 @@ const Modal = ({ name, overview, setShowModal, backdrop }) => {
                     </svg>
                 </div>
 
-
                 <div className="modal-content-inner">
                     <div className='poster' style={{
                         backgroundSize: 'contain',
@@ -25,25 +24,13 @@ const Modal = ({ name, overview, setShowModal, backdrop }) => {
                         backgroundImage: `url(https://image.tmdb.org/t/p/original${backdrop})`,
                         backgroundRepeat: "no-repeat"
                     }}>
-                        <div className='overviewModal'>
-                            <div className='btn-play-modal'>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 24" width="28" height="24">
-                                    <path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path>
-                                </svg>
-                                <span>Assistir</span>
-                            </div>
-                        </div>
-
                     </div>
-
                     <div className='pos-post'>
                         <h2>{name}</h2>
                         <p>{overview}</p>
                         <p>Lembrando que o projeto é apenas um Clone, não é possível assistir nenhum filme.</p>
                         <p>Todos os direitos são revervados a Netflix.</p>
-
                     </div>
-
                 </div>
             </div>
         </div >
